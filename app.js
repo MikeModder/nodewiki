32ce4d8789cba0f57cebc1adbd564f8d5ced8bbf
MikeModder <15313620+MikeModder@users.noreply.github.com>
2017-11-21 20:54:21 -0500

    Switch to SQLite3. it seems to be (somewhat) working now!

--- app.js
+++ app.js
@@ -2,17 +2,30 @@
 const app = require('express')();
 const path = require('path');
 const bodyParser = require('body-parser');
-const nedb = require('nedb-promises');
+const sqlite3 = require('sqlite3');
 const ipAddr = require('ip').address();
+const readline = require('readline');
+
+/* Configure realine */
+readline.emitKeypressEvents(process.stdin);
+process.stdin.setRawMode(true);
+
+/* Add key listener and realted code */
+process.stdin.on('keypress', (str, key) => {
+	if(key.ctrl && key.name === 'c'){
+		console.log(`[INFO] Closing database connection and exiting...`);
+		app.locals.pagesDB.close(() => {
+			console.log(`[PAGEDB] Database connection closed.`);
+			process.exit();
+		})
+	}
+});
 
 /* Set some configuration stuff */
 let port = 8080;
-let dbPath = path.join(__dirname, 'data/pages.db');
+let dbPath = path.join(__dirname, 'data/pages_dev.db');
 
-/* load the Articles database  */
-app.locals.pagesDB = new nedb({filename: dbPath});
-
-/* Set up the routes  */
+/* Set up the routes */
 const index = require('./routes/index');
 const page = require('./routes/page');
 
@@ -20,30 +33,22 @@ const page = require('./routes/page');
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-
-/* Use middleware  */
+/* Use middleware */
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-/* Use the routes  */
+/* Use the routes */
 app.use('/', index);
 app.use('/page', page);
 
-/* Tell Expres to listen on our chosen port  */
-app.listen(port, function(){
+/* load the Articles database */
+app.locals.pagesDB = new sqlite3.Database(dbPath, (err) => {
+	if(err) throw err;
+	console.log(`[PAGEDB] Database loaded without error!`);
+});
+
+/* Tell Expres to listen on our chosen port */
+app.listen(port, () => {
 	console.log(`[INFO] Server listening on port ${port}. The current IP is ${ipAddr}`);
-	app.locals.pagesDB.load()
-		.then(function(err){
-			console.log(`[PAGEDB] Database loaded and ready!`);
-		})
-		.then(function(){
-			app.locals.pagesDB.count({})
-				.then(function(count){
-					console.log(`[PAGEDB] There are currently ${count} pages!`);
-				});
-		})
-		.catch(function(err){
-			console.log(`[PAGEDB] Error during inital setup. \n${err}`);
-			throw err;
-		});
+
 })
--- routes/page.js
+++ routes/page.js
@@ -6,18 +6,18 @@ let pagesDir = '../pages';
 
 router.get('/', function(req, res){
 	res.render('page_list', {serverTime: Date.now()});
-})
+});
 
 router.get('/edit/:pagename', function(req, res){
 	let pagename = req.params.pagename;
 	if(!pagename){
-		res.render('error', {msg: 'Page not found!'});
+		res.render('error', {msg: 'You must specify a page name!'});
 	}
-	//let
-	res.render('edit_page', {pagename: pagename});
-})
 
-router.get('/p/:pagename', function(req, res){
+	res.render('edit_page', {pagename: pagename});
+});
+
+router.get('/:pagename', function(req, res){
 	let pagename = req.params.pagename;
 	if(!pagename){
 		res.redirect('/');
@@ -25,54 +25,25 @@ router.get('/p/:pagename', function(req, res){
 	let cleaned = pagename.split(' ').join('_').toLowerCase();
 	let pagesDB = req.app.locals.pagesDB;
 
-	pagesDB.findOne({title: cleaned})
-			.then(function(found){
-				//Test timeout. Should wait for variable chnage.
-				let found_cache = found;
-				console.log(`[WAIT] Waiting for variable change...`);
-				function waitForChange(){
-					if(found===found_cache){
-						console.log(`[DBG] found: ${found} found_cache: ${found_cache}`);
-						setTimeout(waitForChange, 50);
-					}
-				}
-				console.log(`[WAIT] Variables changed!`);
+	console.log(`cleaned name: ${cleaned}`)
 
-				console.log(`found: ${found}`);
-				if(found === null){
-					throw new Error('found is null!');
-					res.render('error', {msg: 'Page not found!'});
-				}
-				console.log(`Value of found: ${found}`);
-				res.render('page', {title: found.title, author: found.author, createdAt: found.createdAt, content: found.content});
-				console.log('[TEST] test')
-			})
-		.catch(function(err){
-			throw new Error(err);
-			console.log(`[PAGEDB] Error getting and rendering a page!\n${err}`);
-		});
+	pagesDB.get('SELECT id, name, author, content, createdAt FROM pages WHERE cleanName = ?', [cleaned], (err, rows) => {
+		if(err) throw err;
+		if(!rows) {
+			console.log(`Value of rows: ${rows}`)
+			res.render('error', {msg: 'Page not found in the databse!'});
+		}
+		let pageId = rows.id;
+		let title = rows.name;
+		let author = rows.author;
+		let content = rows.content;
+		let createdAt = rows.createdAt;
+		//res.send(rows);
+		res.render('page', {pageId: pageId, title: title, author: author, content: content, createdAt: createdAt});
+	})
 
-	res.render('error', {msg: 'If you see this page, something went horribly wrong!'});
-	throw new Error('Somehow a user escaped!');
-})
-
-router.get('/exp', function(req, res){
-	let pagesDB = req.app.locals.pagesDB;
-	let articleData = {
-		title: 'hello_world',
-		author: 'MikeModder',
-		crreatedAt: 'someday',
-		content: 'I am the content!'
-	}
-	pagesDB.insert(articleData)
-		.then(function(doc){
-			console.log(`[PAGEDB] Inserted test page. ID: ${doc._id}`);
-			res.redirect('/');
-		})
-		.catch(function(err){
-			console.log(`[PAGEDB] Error inserting test data!`);
-			throw err;
-		})
-})
+	//res.render('error', {msg: 'If you see this page, something went horribly wrong!'});
+	//throw new Error('Somehow a user escaped!');
+});
 
 module.exports = router;
